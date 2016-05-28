5b181146498bf07787504f1c7d092cd4f0f99a26
Arttu H <arttuhut@gmail.com>
2016-05-28 10:44:39 +0300

    First version.
    Plug and .py files. Fetches the web page, parses json and returns the
    message in quite ugly format.

---
+++ lounas.py
@@ -0,0 +1,25 @@
+from errbot import BotPlugin, botcmd
+import requests, json
+
+class Lounas(BotPlugin):
+    """Errbot plugin for getting the lunch menu for today at a certain  hardcoded restaurant. The restaurant is Amica Oulu VTT."""
+
+    @botcmd
+    def lounas(self, msg, args):
+        """Get webpage, parse and return"""
+        
+        try:
+
+            url = "http://www.amica.fi/modules/json/json/Index?costNumber=3587&language=fi"
+            menu = requests.get(url)
+            parsedMenu = json.loads(menu.text)
+            menulist = []
+            
+            #in case different amount of items, will create exception an
+            for i in range(0,5):
+                menulist.append(str(parsedMenu['MenusForDays'][0]['SetMenus'][i]['Components']))
+        except:
+            pass        
+        output =''.join(menulist)
+        
+        return output
